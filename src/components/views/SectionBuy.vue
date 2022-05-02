ceef5f997df41ba15c762645d825c348d3d19285
Alejandra Jiménez Rocha <alejandra.jimenez@dgtid.oaxaca.gob.mx>
2022-05-02 17:15:07 -0500

    Modificación página estática

---
+++ src/components/views/SectionBuy.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="sectionbuy">
+    <div><br><br></div>
+		<h1 class="text-center pb-6 text-3xl md:text-4xl lg:text-5xl font-semibold text-indigo-500">
+        <strong>Explora y aprende</strong>
+        </h1>
+		<p class="text-indigo-600 text-center lg:px-24">
+        Accede a nuestros mejores cursos y mejora en tu vida profesional
+        </p>
+		<div class="row">
+            <div class="col">
+                <section style="width:100%">
+                    <div align="center">
+                        <img class="logo-curso-b" src="@/assets/img/videostatic.png" alt="videostatic" width="280" height="170" />
+                    </div>
+                    <div style="margin: 0 auto; width:250px;">
+                        <strong>Lorem ipsum</strong>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-default btn-sm" style="background-color:orange; border-color:orange; color:white">
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>   Inscribirme 
+                            <strong>  $450MX</strong>
+                        </button>
+                    </div>
+                </section>
+            </div>
+            <div class="col">
+                <section style="width:90%">
+                    <div align="center">
+                        <img class="logo-curso-b" src="@/assets/img/videostatic.png" alt="videostatic" width="280" height="170" />
+                    </div>
+                    <div style="margin: 0 auto; width:250px;">
+                        <strong>Lorem ipsum</strong>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-default btn-sm" style="background-color:orange; border-color:orange; color:white">
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>   Inscribirme 
+                            <strong>  $450MX</strong>
+                        </button>
+                    </div>
+                </section>
+            </div>
+            <div class="col">
+                <section style="width:90%">
+                    <div align="center">
+                        <img class="logo-curso-b" src="@/assets/img/videostatic.png" alt="videostatic" width="280" height="170" />
+                    </div>
+                    <div style="margin: 0 auto; width:250px;">
+                        <strong>Lorem ipsum</strong>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-default btn-sm" style="background-color:orange; border-color:orange; color:white">
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>   Inscribirme 
+                            <strong>  $450MX</strong>
+                        </button>
+                    </div>
+                </section>
+            </div>
+            <div class="col">
+                <section style="width:90%">
+                    <div align="center">
+                        <img class="logo-curso-b" src="@/assets/img/videostatic.png" alt="videostatic" width="280" height="170" />
+                    </div>
+                    <div style="margin: 0 auto; width:250px;">
+                        <strong>Lorem ipsum</strong>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                        <p align="left">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunut ut laoreet dolore magna
+                        </p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-default btn-sm" style="background-color:orange; border-color:orange; color:white">
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>   Inscribirme 
+                            <strong>  $450MX</strong>
+                        </button>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div>
+        <br>
+        </div>
+	</div>
+
+</template>
+
+<script>
+export default {
+  name: "SectionBuy",
+};
+</script>
