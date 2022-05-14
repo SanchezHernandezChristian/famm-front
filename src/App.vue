6a2be2c40cc444b7cbf345305b1ad1bff311351e
christian.sanchez3 <christian.sanchez@umvel.com>
2022-05-14 05:20:33 -0500

    Soluccion a errores y modificacion vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <!--<div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>-->
-    <router-view/>
+    <v-app>
+      <router-view />
+    </v-app>
   </div>
 </template>
+<style>
+.v-btn {
+  text-transform: none !important;
+}
+</style>
--- src/components/views/Navbar.vue
+++ src/components/views/Navbar.vue
@@ -1,372 +1,147 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid" style="height: 60px">
-      <div>
-        <img src="@/assets/img/logo.png" alt="FAMM" width="140" height="40" />
-      </div>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul style="margin-top: 20px">
-          <li class="lista1 primero">
-            <a>Cursos</a>
-            <ul id="list-cursos">
-              <li class="lista2">
-                TODOS LOS CURSOS
-                <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Atención al Cliente</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Atención a Comensales</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Bordado Regional</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Bordado Textil</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Contabilidad Básica</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Corte y confección básico Módulo I</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Corte y confección básico Módulo II</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Corte y confección básico Módulo III</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cultivo de hortalizas en casa</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cultivo de hongos seta en casa módulo I</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cultivo de hongos seta en casa módulo II</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Deshilado Módulo I</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Elaboración de Cremas de Mezcal</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Elaboración de cartas descriptivas para impartir cursos a
-                  distancia</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Elaboración de materiales didácticos digitales</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Excel Básico</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Excel Intermedio</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Figuras con Papel Maché</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Formación de Banda de Música Mod. I</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Formación de Banda de Música Mod. II</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Formación de Banda de Música Mod. III</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Formación de Banda de Música Mod. IV</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Formulación de proyectos productivos</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Guitarra Popular mod. I</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cursos nuevos</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cursos cerca de tu localidad</router-link
-                >
-              </li>
-            </ul>
-          </li>
-          <li class="lista1">
-            <a>Especialidades</a>
-            <ul>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Gestión y Venta de Servicios Turísticos</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Hotelería</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Informática</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Estilismo y Diseño de Imagen</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Inglés</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Alimentos y Bebidas</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Diseño y Fabricación de Muebles de Madera</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Diseño y Elaboración de cerámica</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Artesanías con fibras Textiles</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Artesanías con Pastas, Pinturas y Acabados</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Administración</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Unidades de capacitación</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Campos de formación profesional</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cursos mejor valorados</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cursos nuevos</router-link
-                >
-              </li>
-              <li class="lista2">
-                <router-link
-                  style="text-decoration: none; color: inherit"
-                  to="/especialidad"
-                  >Cursos cerca de tu localidad</router-link
-                >
-              </li>
-              <li class="lista2">
-                TODAS LAS ESPECIALIDADES
-                <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <form class="d-flex">
-          <div class="input-group m-4">
-            <span class="input-group-text" id="basic-addon1"
-              ><i class="fa fa-search"></i
-            ></span>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Buscar"
-              aria-label="Buscar"
-              aria-describedby="basic-addon1"
-            />
-          </div>
-        </form>
-      </div>
-    </div>
-    <v-row justify="center" v-if="logged_in">
-      <UserCard :token="token" v-on:user_action="userAction" />
-    </v-row>
-    <v-row justify="center" v-else>
-      <v-col md="6">
-        <Login v-on:user_action="userAction" />
-      </v-col>
-      <v-col md="6">
-        <Registro />
-      </v-col>
-    </v-row>
-  </nav>
+  <v-app-bar fill-height>
+    <v-app-bar absolute color="white" class="hidden-sm-and-down">
+      <v-toolbar-title class="col-2"><v-img max-height="25%" max-width="50%" src="@/assets/img/logo.png"></v-img></v-toolbar-title>
+      <v-toolbar-items class="row">
+        <v-col cols="2">
+          <v-list class="p-0">
+            <v-list-group v-for="item in cursos" :key="item.title" v-model="item.active" no-action>
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item class="pl-5" v-for="child in item.cursos" :key="child.title">
+                <v-list-item-content>
+                  <v-list-item-title v-text="child.title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+          </v-list>
+        </v-col>
+        <v-col cols="2">
+          <v-list class="p-0">
+            <v-list-group v-for="item in especialidades" :key="item.title" v-model="item.active" no-action>
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-list-item class="pl-5" v-for="child in item.especialidades" :key="child.title">
+                <v-list-item-content>
+                  <v-list-item-title v-text="child.title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group> </v-list
+        ></v-col>
+        <v-col cols="2"></v-col>
+        <v-col cols="2" class="pt-4">
+          <v-spacer></v-spacer>
+          <v-responsive max-width="260">
+            <v-text-field dense flat hide-details rounded solo-inverted prepend-inner-icon="mdi-magnify"></v-text-field>
+          </v-responsive>
+        </v-col>
+        <v-col cols="4">
+          <v-row justify="center" v-if="logged_in">
+            <UserCard :token="token" v-on:user_action="userAction" />
+          </v-row>
+          <v-row flex v-else>
+            <v-col cols="5" class="ma-2">
+              <Registro />
+            </v-col>
+            <v-col cols="6" class="ma-2">
+              <Login v-on:user_action="userAction" />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-app-bar dark color="white" class="hidden-md-and-up">
+      <v-toolbar-title><v-img max-height="25%" max-width="25%" src="@/assets/img/logo.png"></v-img></v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark v-bind="attrs" v-on="on"> <v-icon>mdi-menu</v-icon></v-btn>
+        </template>
+        <v-card>
+          <v-toolbar flat color="white">
+            <v-toolbar-title><v-img max-height="25%" max-width="25%" src="@/assets/img/logo.png"></v-img></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-toolbar>
+          <v-spacer></v-spacer>
+          <v-responsive max-width="260">
+            <v-text-field mt-3 dense flat hide-details rounded solo-inverted prepend-inner-icon="mdi-magnify"></v-text-field>
+          </v-responsive>
+          <v-row justify="center" v-if="logged_in">
+            <UserCard :token="token" v-on:user_action="userAction" />
+          </v-row>
+          <v-row class="ma-2" v-else>
+            <v-col cols="5" class="ma-2">
+              <Registro />
+            </v-col>
+            <v-col cols="5" class="ma-2">
+              <Login v-on:user_action="userAction" />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-dialog>
+    </v-app-bar>
+  </v-app-bar>
 </template>
 
 <script>
-import Login from "../Login.vue";
-import Registro from "../Registro.vue";
-import UserCard from "../UserCard.vue";
+import Login from '../Login.vue';
+import Registro from '../Registro.vue';
+import UserCard from '../UserCard.vue';
 
 export default {
-  name: "Navbar",
+  name: 'Navbar',
   components: {
     Login,
     Registro,
     UserCard,
   },
   data: () => ({
+    cursos: [
+      {
+        action: 'mdi-ticket',
+        cursos: [
+          { title: 'Todos los cursos' },
+          { title: 'Atención al Cliente' },
+          { title: 'Atención a Comensales' },
+          { title: 'Informática' },
+          { title: 'Excel ' },
+          { title: 'Formación de Banda de Música' },
+          { title: 'Contabilidad' },
+          { title: 'Elaboración de Cremas de Mezcal' },
+        ],
+        title: 'Cursos',
+      },
+    ],
+    especialidades: [
+      {
+        action: 'mdi-ticket',
+        especialidades: [
+          { title: 'Todas las especialidades' },
+          { title: 'Hotelería' },
+          { title: 'Administración' },
+          { title: 'Bordado Regional' },
+          { title: 'Inglés ' },
+          { title: 'Artesanías con fibras Textiles' },
+          { title: 'Estilismo y Diseño de Imagen' },
+          { title: 'Diseño y Elaboración de cerámica' },
+        ],
+        title: 'Especialidades',
+      },
+    ],
     logged_in: false,
-    token: "",
+    token: '',
+    dialog: false,
   }),
   methods: {
     userAction(user_data) {
