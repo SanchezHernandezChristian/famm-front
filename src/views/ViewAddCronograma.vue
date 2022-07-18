f9c9bafd2fcb2b896d3fcdea4adab415712a1eb2
Moises Ruiz <isc.moisesruiz@outlook.com>
2022-07-18 16:34:05 -0500

    CRUD de cronograma

---
+++ src/views/ViewAddCronograma.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MenuAdmin />
+    <FormCronograma />
+    <FooterDashboardAdmin />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FooterDashboardAdmin from "@/components/views/FooterDashboardAdmin.vue";
+import FormCronograma from "@/components/views/FormCronograma.vue";
+import MenuAdmin from "@/components/views/MenuAdmin.vue";
+
+export default {
+  name: "ViewUsers",
+  components: {
+    MenuAdmin,
+    FormCronograma,
+    FooterDashboardAdmin,
+  },
+};
+</script>
