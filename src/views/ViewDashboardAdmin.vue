5708f61157f6d5fa1430a840a1511380fa20df5a
christian.sanchez3 <christian.sanchez@umvel.com>
2022-09-07 12:32:34 -0500

    modificaciones dashboard

--- src/views/ViewDashboardAdmin.vue
+++ src/views/ViewDashboardAdmin.vue
@@ -1,23 +1,23 @@
 <template>
-    <div>
-        <NavbarApp/>
-        <DashboardAdministrativo />
-        <FooterDashboardAdmin />
-    </div>
+  <div>
+    <NavbarApp />
+    <DashboardAdministrativo />
+    <FooterDashboardAdmin />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import DashboardAdministrativo from "@/components/views/DashboardAdministrativo.vue";
-import FooterDashboardAdmin from "@/components/views/FooterDashboardAdmin.vue";
-import NavbarApp from "@/components/views/NavbarApp.vue";
+import DashboardAdministrativo from '@/components/views/DashboardAdministrativo.vue';
+import FooterDashboardAdmin from '@/components/views/FooterDashboardAdmin.vue';
+import NavbarApp from '@/components/views/NavbarApp.vue';
 
 export default {
-    name: "dashboard-admin",
-    components: {
+  name: 'dashboard-admin',
+  components: {
     NavbarApp,
     DashboardAdministrativo,
     FooterDashboardAdmin,
-},
+  },
 };
 </script>
--- src/views/ViewDashboardInstructor.vue
+++ src/views/ViewDashboardInstructor.vue
@@ -1,23 +1,23 @@
 <template>
-    <div>
-        <NavbarApp color="orange" actions="NavOptionsTeacher" />
-        <DashboardInstructor />
-        <FooterDashboardAdmin />
-    </div>
+  <v-container fluid p-0 class="max-height">
+    <NavbarApp color="orange" actions="NavOptionsTeacher" />
+    <DashboardInstructor />
+    <FooterDashboardAdmin />
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import NavbarApp from "@/components/views/NavbarApp.vue";
-import DashboardInstructor from "@/components/views/DashboardInstructor.vue";
-import FooterDashboardAdmin from "@/components/views/FooterDashboardAdmin.vue";
+import NavbarApp from '@/components/views/NavbarApp.vue';
+import DashboardInstructor from '@/components/views/DashboardInstructor.vue';
+import FooterDashboardAdmin from '@/components/views/FooterDashboardAdmin.vue';
 
 export default {
-    name: "ViewDashboardInstructor",
-    components: {
-        NavbarApp,
-        DashboardInstructor,
-        FooterDashboardAdmin,
-    },
+  name: 'ViewDashboardInstructor',
+  components: {
+    NavbarApp,
+    DashboardInstructor,
+    FooterDashboardAdmin,
+  },
 };
 </script>
