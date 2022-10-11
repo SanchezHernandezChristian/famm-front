fe4097e60cf26e5acf2a62a1b994b6c46690c22d
Alejandra Jiménez Rocha <alejandra.jimenez@dgtid.oaxaca.gob.mx>
2022-10-11 00:44:23 -0500

    Cargar alumnos

--- src/components/views/RelacionParticipantesRegistrados.vue
+++ src/components/views/RelacionParticipantesRegistrados.vue
@@ -59,146 +59,126 @@
                   <v-row justify="center" align="center">
                     <br />
                     <h2 style="color: #2b4c7b">
-                      Capturar/Editar relación participantes
+                      Ver/Editar Factibilidad y Justificación
                     </h2>
                   </v-row>
-                  <v-row>
-                    <v-row justify="center" align="center">
-                      <v-layout row justify-center>
-                        <v-flex align-self-center xs4> </v-flex>
-                        <v-flex align-self-center xs2
-                          ><label>Factibilidad y justiticación</label></v-flex
+                  <v-row justify="center" align="center">
+                    <v-layout row justify-center>
+                      <v-flex align-self-center xs2> </v-flex>
+                      <v-flex align-self-center xs3
+                        ><label>Factibilidad y justiticación</label></v-flex
+                      >
+                      <v-flex align-self-center xs3>
+                        <v-col>
+                          <v-text-field
+                            outlined
+                            class="bordeRedondoElement"
+                            v-model="editedItem.detalles"
+                            disabled
+                          ></v-text-field
+                        ></v-col>
+                      </v-flex>
+                      <v-flex align-self-center xs3> </v-flex>
+                    </v-layout>
+                  </v-row>
+                  <v-row justify="center" align="center">
+                    <v-layout row justify-center>
+                      <v-flex align-self-center xs2> </v-flex>
+                      <v-flex align-self-center xs3
+                        ><label>Seleccione un capacitando</label></v-flex
+                      >
+                      <v-flex align-self-center xs3>
+                        <v-col>
+                          <v-select
+                            v-model="selectStudent"
+                            :items="items_full"
+                            item-text="fullnameStudent"
+                            item-value="id"
+                            :rules="rules"
+                            required
+                            return-object
+                            dense
+                            outlined
+                            class="bordeRedondoElement"
+                          ></v-select>
+                        </v-col>
+                      </v-flex>
+                      <v-flex align-self-center xs3> </v-flex>
+                    </v-layout>
+                  </v-row>
+                  <v-row justify="center" align="center" style="height: 65px">
+                    <v-layout row justify-center>
+                      <v-flex align-self-center xs4> </v-flex>
+                      <v-flex align-self-center xs2></v-flex>
+                      <v-flex align-self-center xs2>
+                        <v-btn @click="addAlumno(editedItem)"> Añadir</v-btn>
+                      </v-flex>
+                      <v-flex align-self-center xs2> </v-flex>
+                      <v-flex align-self-center xs2> </v-flex>
+                    </v-layout>
+                  </v-row>
+                  <v-row justify="center" align="center" style="height: 220px">
+                    <v-layout row justify-start>
+                      <v-flex align-self-center xs2> </v-flex>
+                      <v-flex align-self-start xs8>
+                        <v-data-table
+                          :headers="headers_alumno"
+                          :items="items_relacionP"
+                          item-key="nombres"
+                          class="elevation-1"
                         >
-                        <v-flex align-self-center xs3>
-                          <v-col>
-                            <v-text-field
-                              outlined
-                              class="bordeRedondoElement"
-                              v-model="items_cursos.factibilidad.detalles"
-                              disabled
-                            ></v-text-field
-                          ></v-col>
-                        </v-flex>
-                        <v-flex align-self-center xs3> </v-flex>
-                      </v-layout>
-                    </v-row>
-                    <v-row justify="center" align="center">
-                      <v-layout row justify-center>
-                        <v-flex align-self-center xs4> </v-flex>
-                        <v-flex align-self-center xs2
-                          ><label>Seleccione un capacitando</label></v-flex
-                        >
-                        <v-flex align-self-center xs3>
-                          <v-col>
-                            <v-select
-                              v-model="selectStudent"
-                              :items="items_students"
-                              item-text="fullnameStudent"
-                              item-value="id"
-                              :rules="rules"
-                              required
-                              return-object
-                              dense
-                              outlined
-                              class="bordeRedondoElement"
-                            ></v-select>
-                          </v-col>
-                        </v-flex>
-                        <v-flex align-self-center xs3> </v-flex>
-                      </v-layout>
-                    </v-row>
-                    <v-row justify="center" align="center" style="height: 65px">
-                      <v-layout row justify-center>
-                        <v-flex align-self-center xs4> </v-flex>
-                        <v-flex align-self-center xs2></v-flex>
-                        <v-flex align-self-center xs2>
-                          <v-btn text @click="addAlumno">
-                            <small>Añadir</small></v-btn
-                          >
-                        </v-flex>
-                        <v-flex align-self-center xs2> </v-flex>
-                        <v-flex align-self-center xs2> </v-flex>
-                      </v-layout>
-                    </v-row>
-                    <v-row
-                      justify="center"
-                      align="center"
-                      style="height: 220px"
-                    >
-                      <v-layout row justify-start>
-                        <v-flex align-self-center xs4> </v-flex>
-                        <v-flex align-self-start xs6>
-                          <v-data-table
-                            :headers="headers"
-                            :items="items_relacionP"
-                            item-key="fullname"
-                            class="elevation-1"
-                          >
-                            <template v-slot:top>
-                              <v-toolbar flat>
-                                <v-dialog v-model="dialogDelete" width="500">
-                                  <v-card>
-                                    <v-card-title
-                                      class="text-h5 white lighten-2"
-                                    >
-                                      Eliminar alumno
-                                    </v-card-title>
-                                    <v-card-text>
-                                      ¿Estás seguro que quieres eliminar el
-                                      alumno seleccionado? Recuerda que no
-                                      podrás recuperar la información.
-                                    </v-card-text>
-                                    <v-card-actions>
-                                      <v-btn
-                                        outlined
-                                        color="gray"
-                                        class="bordeRedondoElement"
-                                        @click="dialogDelete = false"
-                                      >
-                                        Cancelar
-                                      </v-btn>
-                                      <v-btn
-                                        outlined
-                                        style="
-                                          color: #ffffff;
-                                          background-color: #2b4c7b;
-                                        "
-                                        class="bordeRedondoElement"
-                                        @click="deleteAlumno()"
-                                        >Continuar</v-btn
-                                      >
-                                    </v-card-actions>
-                                  </v-card>
-                                </v-dialog>
-                              </v-toolbar>
-                            </template>
-                            <template v-slot:[`item.actions`]="{ item }">
-                              <v-btn text @click="deleteItem(item)">
-                                <v-icon small>mdi-window-close</v-icon
-                                ><small>Eliminar</small></v-btn
+                          <template v-slot:top>
+                            <v-toolbar flat>
+                              <v-dialog
+                                v-model="dialogDeleteAlumno"
+                                width="500"
                               >
-                            </template></v-data-table
-                          ></v-flex
-                        >
-                        <v-flex align-self-center xs2> </v-flex>
-                      </v-layout>
-                    </v-row>
+                                <v-card>
+                                  <v-card-title class="text-h5 white lighten-2">
+                                    Eliminar alumno
+                                  </v-card-title>
+                                  <v-card-text>
+                                    ¿Estás seguro que quieres eliminar el alumno
+                                    seleccionado? Recuerda que no podrás
+                                    recuperar la información.
+                                  </v-card-text>
+                                  <v-card-actions>
+                                    <v-btn
+                                      outlined
+                                      color="gray"
+                                      class="bordeRedondoElement"
+                                      @click="dialogDeleteAlumno = false"
+                                    >
+                                      Cancelar
+                                    </v-btn>
+                                    <v-btn
+                                      outlined
+                                      class="bordeRedondoElement"
+                                      @click="deleteAlumno()"
+                                      >Continuar</v-btn
+                                    >
+                                  </v-card-actions>
+                                </v-card>
+                              </v-dialog>
+                            </v-toolbar>
+                          </template>
+                          <template v-slot:[`item.actions`]="{ item }">
+                            <v-btn text @click="deleteItem(item)">
+                              <v-icon small>mdi-window-close</v-icon
+                              ><small>Eliminar</small></v-btn
+                            >
+                          </template></v-data-table
+                        ></v-flex
+                      >
+                      <v-flex align-self-center xs2> </v-flex>
+                    </v-layout>
                   </v-row>
                   <v-card-actions>
                     <v-btn
                       outlined
                       color="gray"
                       class="bordeRedondoElement"
-                      @click="
-                        editParticipante(
-                          editedItem.nombres,
-                          editedItem.apellido_paterno,
-                          editedItem.apellido_materno,
-                          editedItem.sexo,
-                          editedItem.telefono,
-                          editedItem.celular
-                        )
-                      "
+                      @click="editFactibilidad(editedItem)"
                       >Guardar cambios</v-btn
                     >
                     <v-btn
@@ -215,11 +195,11 @@
               <v-dialog v-model="dialogDelete" width="500">
                 <v-card>
                   <v-card-title class="text-h5 white lighten-2">
-                    Eliminar relación participante
+                    Eliminar relación de participantes
                   </v-card-title>
                   <v-card-text>
-                    ¿Estás seguro que quieres eliminar la relación del
-                    participante seleccionado? Recuerda que no podrás recuperar
+                    ¿Estás seguro que quieres eliminar la relación de
+                    participantes seleccionada? Recuerda que no podrás recuperar
                     la información.
                   </v-card-text>
                   <v-card-actions>
@@ -274,6 +254,7 @@ export default {
     datarespuestaDelete: [],
     dialog: false,
     dialogDelete: false,
+    dialogDeleteAlumno: false,
     dialogEdit: false,
     dialogCaptura: false,
     //Elementos para la tabla
@@ -286,7 +267,22 @@ export default {
       },
       { text: "Clave del curso", value: "clave_curso" },
       { text: "Horas", value: "duracion_horas" },
-      { text: "Especialidad", value: "nombre_especialidad" },
+      // { text: "Especialidad", value: "item_especialidad.nombre_especialidad" },
+      { text: " ", value: "actions" },
+    ],
+    headers_alumno: [
+      {
+        text: "Nombre del alumno",
+        align: "start",
+        sortable: false,
+        value: "nombres",
+      },
+      { text: "Apellido P.", value: "apellido_paterno" },
+      { text: "Apellido M.", value: "apellido_materno" },
+      { text: "Edad", value: "edad" },
+      { text: "Sexo", value: "sexo" },
+      { text: "Teléfono", value: "telefono" },
+      // { text: "Especialidad", value: "item_especialidad.nombre_especialidad" },
       { text: " ", value: "actions" },
     ],
     participantes: [],
@@ -304,27 +300,44 @@ export default {
     relacionCapturada: false,
     items_factibilidades: [],
     items_relacionP: [],
-    selectStudent: {},
+    selectStudent: {
+      id: null,
+      nombres: "",
+      primer_apellido: null,
+      segundo_apellido: null,
+      email: "",
+      user_name: "",
+      idCentro_capacitacion: null,
+      idRol: null,
+      nombre_rol: "",
+    },
+    item_especialidad: [],
+    items_full: [],
   }),
 
   async mounted() {
     try {
-      //Pruebas
-      const responseProfile = await AuthService.getProfile();
-      this.user.idUnidad = responseProfile.idCentro_capacitacion;
-      const response6 = await AuthService.getAllAssignUnidad(
-        this.user.idUnidad
-      );
-      this.items_cursos = response6.cursos;
+      const response2 = await AuthService.getCursoFactibilidadIdCenter();
+      this.items_cursos = response2.cursos;
+      console.log("item_cursos", this.item_cursos);
       const response3 = await AuthService.fetchUsers();
       this.items_students = response3.data.filter((item) => {
         if (item.idRol == 1) return item;
       });
-      console.log("item_students", this.items_students);
-      //const response2 = await AuthService.getCursoFactibilidadIdCenter();
-      //this.items_cursos = response2.data;
-      const responseFact = await AuthService.getAllFactibilidades();
-      this.items_factibilidades = responseFact.data;
+      this.items_full = this.items_students.map((item) => {
+        item.fullnameStudent = `${item.nombres} ${item.primer_apellido} ${item.segundo_apellido}`;
+        return item;
+      });
+      console.log("items_full", this.items_full);
+      // const responseProfile = await AuthService.getProfile();
+      // this.user.idUnidad = responseProfile.idCentro_capacitacion;
+      // const response3 = await AuthService.fetchUsers();
+      // this.items_students = response3.data.filter((item) => {
+      //   if (item.idRol == 1) return item;
+      // });
+      // console.log("item_students", this.items_students);
+      // const responseFact = await AuthService.getAllFactibilidades();
+      // this.items_factibilidades = responseFact.data;
     } catch (error) {
       console.log(error);
     }
@@ -446,7 +459,7 @@ export default {
       console.log("editedItem ", this.editedItem);
       this.dialogEdit = true;
       console.log("item edit ", item);
-      this.editId = this.editedItem.factibilidad.idFactibilidad;
+      this.editId = this.editedItem.idFactibilidad;
       console.log("editedItem.idFactibilidad ", this.editId);
       this.rellenar(this.editedItem);
     },
@@ -455,32 +468,49 @@ export default {
       console.log("deleteIndex ", this.editedIndex);
       this.editedItem = Object.assign({}, item);
       console.log("deleteItem ", this.editedItem);
-      this.deleteId = this.editedItem.factibilidad.idFactibilidad;
+      this.deleteId = this.editedItem.idFactibilidad;
       console.log("editedItem.idFactibilidad ", this.deleteId);
       this.dialogDelete = true;
     },
 
     async rellenar(data_p) {
-      this.items_students = response.data.map((item) => {
-        item.fullnameStudent = `${item.nombres} ${item.primer_apellido} ${item.segundo_apellido}`;
-        return item;
-      });
-      let idFact = data_p.factibilidad.idFactibilidad;
+      // const response_es = await AuthService.getEspecialidad(
+      //   data_p.idEspecialidad
+      // );
+      // this.item_especialidad = response_es.data;
+      let idFact = data_p.idFactibilidad;
       const response = await AuthService.getAllParticipantes();
       // this.participantes = response.data;
       // console.log("relacionParticipantes", this.participantes);
       this.items_relacionP = response.data.filter((item) => {
         if (item.idFactibilidad == idFact) return item;
       });
-      this.items_relacionP = response.data.map((item) => {
-        item.fullname = `${item.nombre} ${item.apellido_paterno} ${item.apellido_materno}`;
-        return item;
-      });
+      console.log("items_relacionparticipantes ",this.items_relacionP);
+      // this.items_relacionP = response.data.map((item) => {
+      //   item.fullname = `${item.nombre} ${item.apellido_paterno} ${item.apellido_materno}`;
+      //   return item;
+      // });
     },
 
     async deleteAlumno() {},
 
-    async addAlumno() {},
+    async addAlumno(data) {
+      let alumno = {
+        nombres: this.selectStudent.nombres,
+        apellido_paterno: this.selectStudent.primer_apellido,
+        apellido_materno: this.selectStudent.segundo_apellido,
+        sexo: "N",
+        telefono: null,
+        celular: null,
+        idFactibilidad: data.idFactibilidad,
+        numero_identificacion: this.selectStudent.id,
+        edad: null,
+      };
+      console.log("data_alumno ",alumno);
+      const responseAdd = await AuthService.addRelacionParticipante(alumno);
+      let statusadd = responseAdd.data;
+      console.log(statusadd);
+    },
   },
 };
 </script>
